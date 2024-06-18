d2cf1eaac3cd991701994cf6439b1eba191693ea
wsheaven <ws.heaven@gmail.com>
2024-06-17 21:40:46 -0600

    Simulate a db to make api calls to and load data from there

--- src/App.js
+++ src/App.js
@@ -4,34 +4,61 @@ import SearchItem from './SearchItem';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
+
 
 function App() {
 
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('ShoppingList')))
+  const API_URL = "http://localhost:3500/items" 
+
+  const [items, setItems] = useState([])
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
 
-  const setAndSaveItems = (newItems) => {
-    setItems(newItems)
-    localStorage.setItem('ShoppingList', JSON.stringify(newItems))
-  }
- 
+  /* useEffect will do something when the state of another changes, so when 
+  items updates in anyway it will log it out. 
+  useEffect(() => {
+    console.log('updating item state')
+  }, [items])
+  */
+  useEffect(() => {
+    
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error("Did not receive data")
+        const listItems = await response.json()
+        setItems(listItems)
+        setFetchError(null)
+      } catch (err) {
+        console.log(err.stack)
+        setFetchError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    (async () => await fetchItems())()
+  }, [])
+
+
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1; 
     const myNewItem = {id, checked: false, item}
     const listItems = [...items, myNewItem]
-    setAndSaveItems(listItems)
+    setItems(listItems)
   }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
-    setAndSaveItems(listItems)
+    setItems(listItems)
   }
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
-    setAndSaveItems(listItems)
+    setItems(listItems)
   }
 
   const handleSubmit = (e) => {
@@ -57,11 +84,15 @@ function App() {
       search={search}
       setSearch={setSearch}
       />
-      <Content
+      <main>
+        {isLoading && <p>Loading Items...</p>}
+        {fetchError && <p style={{color: 'red'}}>Error: {fetchError}</p>}
+      {!fetchError && !isLoading && <Content
         items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer
         length={items.length}
       />
